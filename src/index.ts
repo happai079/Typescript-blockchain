a379a3024543ab6a8c0cfdc37cc2265019372144
hayoung <gksyddl22@gmail.com>
2021-05-25 23:38:46 +0900

    class - 속성/ insterface와 달리 js로 컴파일 됨

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
-interface Human {
-    name: string;
-    age: number;
-    gender: string;
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name: string, age: number, gender: string){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
-const person = {
-    name: 'hayoung',
-    age: 22,
-    gender: 'female'
-}
+const so = new Human("So", 20, "female");
 
 const sayHi = (person: Human): string => {
     return `Hello ${person.name} you are ${person.age}, you are ${person.gender}!`;
 }
 
-console.log(sayHi(person));
+console.log(sayHi(so));

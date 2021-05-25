c045afd96ef9d6a3da2174e6595bcf6c8a5d7f13
hayoung <gksyddl22@gmail.com>
2021-05-25 23:02:23 +0900

    what is type?

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-console.log("hello");
+"use strict";
+var sayHi = function (name, age, gender) {
+    console.log("Hello " + name + "! you are " + age + ", you are " + gender);
+};
+sayHi("hayoung", 20, "female");
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,5 @@
-console.log("hello");
+const sayHi = (name: string, age: number , gender: string) => {
+    console.log(`Hello ${name}! you are ${age}, you are ${gender}`);
+}
+
+sayHi("hayoung", 20, "female");

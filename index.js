c12893bff651931a176e13f52740b7d71c512091
hayoung <gksyddl22@gmail.com>
2021-05-25 23:21:48 +0900

    tscofig 파일 수정 및 tsc-watdh 설치

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 var sayHi = function (name, age, gender) {
-    console.log("Hello " + name + "! you are " + age + ", you are " + gender);
+    return ("Hello " + name + "! you are " + age + ", you are " + gender);
 };
 sayHi("hayoung", 20, "female");
 //# sourceMappingURL=index.js.map
--- index.ts
+++
@@ -1,5 +0,0 @@
-const sayHi = (name: string, age: number , gender: string) => {
-    console.log(`Hello ${name}! you are ${age}, you are ${gender}`);
-}
-
-sayHi("hayoung", 20, "female");
